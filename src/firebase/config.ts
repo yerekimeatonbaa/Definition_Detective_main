4bc95a47383365ef02a62ac993f63ece7488a088
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-10-24 13:16:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4536174912-ee6ca",
+  "appId": "1:835318680713:web:ed6b1fbf0a8684b8a08a58",
+  "apiKey": "AIzaSyDdx1Qxmt89XJmQTLYuUw96E32_Yu07iPs",
+  "authDomain": "studio-4536174912-ee6ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "835318680713"
+};
