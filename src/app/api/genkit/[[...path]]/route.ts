1154099008cdedff8f9a2073479bf8a334637ca2
Your Name <yerekimeatonbaa@gmail.com>
2025-10-29 15:33:14 +0000

    starting build "e7a3f240-c54d-46cb-8080-9bd35c9233cf" FETCHSOURCE BUILD

--- src/app/api/genkit/[[...path]]/route.ts
+++ src/app/api/genkit/[[...path]]/route.ts
@@ -1,5 +1,6 @@
-import {createApiHandler} from '@genkit-ai/next';
+
+import { api } from '@genkit-ai/next/api';
 import '@/ai/flows/smart-word-hints';
 import '@/ai/flows/game-sounds-flow';
 
-export const {GET, POST} = createApiHandler();
+export const { GET, POST } = api();
