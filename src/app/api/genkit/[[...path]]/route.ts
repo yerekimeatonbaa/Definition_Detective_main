4d7b317fce9d2b1bfcd0c773b5dcc46ad4671e04
Tonbra Benjamin Yerekimea <yerekimeatonbaa@gmail.com>
2025-11-02 04:34:54 +0000

    starting build "67b335aa-ad72-41cb-8b42-da2d64853dcd" FETCHSOURCE BUILD

--- src/app/api/genkit/[[...path]]/route.ts
+++ src/app/api/genkit/[[...path]]/route.ts
@@ -1,6 +1,6 @@
 
-import { createApiHandler } from '@genkit-ai/next';
+import { defineDotprompt } from 'genkit';
 import '@/ai/flows/smart-word-hints';
 import '@/ai/flows/game-sounds-flow';
 
-export const { GET, POST } = createApiHandler();
+export const { GET, POST } = defineDotprompt();
